c5c812972c24a46f698a7e4a4e52de54716d921f
Hector Mendoza <roar109@gmail.com>
2016-04-20 23:02:58 -0500

    add users model

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-require('./lib/configuration');
+const config = require('./lib/configuration').config;
+const UserDelegate = require('./lib/users').UserDelegate;
+
+console.log(config.botName);
+console.log(UserDelegate.getUsersFromChannel('url','id'));
