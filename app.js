f8554327e7d479e40597a9c8333de13cc0e31f84
Hector Mendoza <hmendoza@24hourfit.com>
2016-04-21 19:38:25 -0500

    Add complete structure

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const config = require('./lib/configuration').config;
 const UserDelegate = require('./lib/users').UserDelegate;
+const TimeHandler = require('./lib/time').TimeHandler;
+const ExerciseHandler = require('./lib/excercise').ExerciseHandler;
 
-console.log(config.botName);
-console.log(UserDelegate.getUsersFromChannel('url','id'));
+TimeHandler.loadConfig(config);
+UserDelegate.loadConfig(config);
+ExerciseHandler.loadConfig(config);
+
+UserDelegate.loadUsersFromChannel(function(){
+  console.log("Loaded "+UserDelegate.userCache.length+" users");
+  main();
+});
+
+function main(){
+  if(TimeHandler.isWorkingTime()){
+    console.log("Working Time");
+    const excercise = ExerciseHandler.selectExerciseAndStartTime();
+    ExerciseHandler.assignExercise(excercise, UserDelegate);
+  }else{
+    console.log("No working Time, waiting 5 min...");
+    setTimeout(main, 1000*60*5);
+  }
+}
