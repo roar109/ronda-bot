d56d365c8afe9dd88bf6c969b8a06f0d4ba3ca8b
Hector Mendoza <roar109@gmail.com>
2016-04-21 22:55:14 -0500

    add callouts

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const config = require('./lib/configuration').config;
 const UserDelegate = require('./lib/users').UserDelegate;
 const TimeHandler = require('./lib/time').TimeHandler;
 const ExerciseHandler = require('./lib/excercise').ExerciseHandler;
+const CalloutHandler = require('./lib/callout').CalloutHandler;
 
 TimeHandler.loadConfig(config);
 UserDelegate.loadConfig(config);
 ExerciseHandler.loadConfig(config);
+CalloutHandler.loadConfig(config);
 
 UserDelegate.loadUsersFromChannel(function(){
   console.log("Loaded "+UserDelegate.userCache.length+" users");
@@ -15,8 +17,11 @@ UserDelegate.loadUsersFromChannel(function(){
 function main(){
   if(TimeHandler.isWorkingTime()){
     console.log("Working Time");
-    const excercise = ExerciseHandler.selectExerciseAndStartTime();
-    ExerciseHandler.assignExercise(excercise, UserDelegate);
+    ExerciseHandler.addCalloutHandler(CalloutHandler);
+    ExerciseHandler.callAssignExcercise = function(excercise){
+        ExerciseHandler.assignExercise(excercise, UserDelegate);
+    };
+    ExerciseHandler.selectExerciseAndStartTime();
   }else{
     console.log("No working Time, waiting 5 min...");
     setTimeout(main, 1000*60*5);
